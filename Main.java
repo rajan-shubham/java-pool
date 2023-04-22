a2687df168ef697b5d445c52411f95d1ab8ff144
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-22 18:46:17 +0530

    learning input & output KKlec4

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hey Shubham");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
