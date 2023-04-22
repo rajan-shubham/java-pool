592bbeae15d75fa6a830e50641f4cf9553444d10
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-22 19:15:38 +0530

    learning primitives datatypes KKlec4

---
+++ Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives{
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 97.59f;
+        double largeDecimalNumbers = 456748.6786;
+        long largeInteger = 248938490234l;
+        boolean check = false;
+    }
+}
