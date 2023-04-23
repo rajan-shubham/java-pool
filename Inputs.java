809436363b30a855560bde01c01aba680541b421
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-23 11:15:28 +0530

    learn input&output in lec4 KK

---
+++ Inputs.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some inputes : ");
+        int rollno = input.nextInt();
+        System.out.println("Ypur roll number is " + rollno);
+    }
+}
