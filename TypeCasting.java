f3f608fcbf5b130e202b6bcd23ae39606554e9c5
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-23 14:11:41 +0530

    casting&typeConversion prog lec4 KK

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TypeCasting{
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+    }
+}
