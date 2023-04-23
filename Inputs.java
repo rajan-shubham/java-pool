d9b0e68fe82fd31830b57550071a9d7e46d3ae83
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-04-23 13:13:54 +0530

    learn inpout&output lec4 KK

--- Inputs.java
+++ Inputs.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter some inputes : ");
-        int rollno = input.nextInt();
-        System.out.println("Ypur roll number is " + rollno);
+       // System.out.print("Please enter some inputes : ");
+       // int rollno = input.nextInt();
+       // System.out.println("Ypur roll number is " + rollno);
+       // int a = 234_000_000;
+       // System.out.println(a);
+       // String name = input.nextLine();
+       // System.out.println(name); 
+        float marks = input.nextFloat();
+        System.out.println(marks);
     }
 }
