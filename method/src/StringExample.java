e80035343d1fa652c2136cbabc0e194bdacf7622
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-11-30 12:06:21 +0530

    < Small greeting function >

---
+++ method/src/StringExample.java
@@ -0,0 +1,11 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet(){
+        String greeting = "how are you";
+        return greeting;
+    }
+}
