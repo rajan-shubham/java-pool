197a5c69aec8df6d44a247aa4c5d4d38e0e75e7a
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-12-07 22:31:12 +0530

    < Arrays Syntax >

---
+++ arrays_lec_8/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Why we need array ?
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "shubham kumar";
+
+        // Q: store 5 roll no.
+        int roll1 = 23;
+        int roll2 = 55;
+        int roll3 = 18;
+
+        // Array : it is an collection of same datatype's element
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        int[] rnos =  new int[5];
+        // or directly
+        int[] rnos2 = {23, 12, 45, 32, 15};
+    }
+}
