d549742f76bdd3836dca9a6e4f743e042fdb3d41
rajan-shubham <SHUBHAMRAJAN@proton.me>
2023-12-15 08:57:28 +0530

    < Arrays basics >

--- arrays_lec_8/src/Main.java
+++ arrays_lec_8/src/Main.java
@@ -12,11 +12,22 @@ public class Main {
         int roll2 = 55;
         int roll3 = 18;
 
-        // Array : it is an collection of same datatype's element
+        // Array : it is a collection of same datatype element
         // syntax
         // datatype[] variable_name = new datatype[size];
         int[] rnos =  new int[5];
         // or directly
-        int[] rnos2 = {23, 12, 45, 32, 15};
+        int[] rnos2 = {23, 12, 45, 32, 15}; // all datatype should be same
+
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialisation : actually here object is being created in the memory (heap)
+
+        ros[1] = 33;
+        System.out.println(ros[1] + ros[0]);
+
+        // new keyword : basically just means it is used to create an object
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
     }
 }
