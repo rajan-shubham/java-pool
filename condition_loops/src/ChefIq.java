5501709f321e170d3805630721b858da2bf99452
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-01-01 15:27:42 +0530

    < IQ is greater then einstin of not >

---
+++ condition_loops/src/ChefIq.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefIq
+{
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        if((x + 7) > 170){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+    }
+}
