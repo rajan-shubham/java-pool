2dc27da08eee8dfb5a47e33d09a2e24ef6dc052c
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-02 21:20:38 +0530

    Linear Search using array of int & string

---
+++ LinSearch/src/SearchInStrings.java
@@ -0,0 +1,30 @@
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "shubham";
+        char target = 'u';
+        System.out.println(search(name, target));
+    }
+
+    static boolean search2(String str, char target){
+        if (str.length() == 0){
+            return false;
+        }
+        for(char ch : str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean search(String str, char target){
+        if (str.length() == 0){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
