d160b489df9d8324b4655e425faa2009da1f8833
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-29 06:11:40 +0530

    java basics of string

---
+++ Strings_12/src/Comparision.java
@@ -0,0 +1,17 @@
+public class Comparision {
+    public static void main(String[] args) {
+        String a = "Kunal";
+        String b = "Kunal";
+        String c = a;
+        System.out.println(c == b);
+
+        // ==
+        // System.out.println(a == b);
+
+        String name1 = new String("Kunal");
+        String name2 = new String("Kunal");
+
+//        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+    }
+}
---
+++ Strings_12/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int num = 10;
+        String name = "Shubham Kumar";
+//        System.out.println(name);
+        String a = "kunal";
+        System.out.println(a);
+        a = "Rajan";
+        System.out.println(a);
+    }
+}
