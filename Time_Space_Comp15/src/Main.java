5b9f9f5bee2da6f1e9a35baccc32b51dfd914278
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-07 14:36:56 +0530

    Fibonacci mathematical formula

--- Time_Space_Comp15/src/Main.java
+++ Time_Space_Comp15/src/Main.java
@@ -3,6 +3,17 @@ public class Main {
         System.out.println("Hello world!");
         int ans = fibo(15);
         System.out.println(ans);
+
+        for (int i = 0; i< 11; i++){
+            System.out.println(fiboFormula(i));
+        }
+
+        System.out.println(fiboFormula(50));
+    }
+
+    static int fiboFormula(int n){
+        // just for demo, use long instead
+        return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
 
     static int fibo(int n){
