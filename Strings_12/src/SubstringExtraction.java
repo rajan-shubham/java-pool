54a2e2aee0c70351a7c37b72b81d34b573bb897e
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-04-26 21:45:38 +0530

    SubstringExtraction

---
+++ Strings_12/src/SubstringExtraction.java
@@ -0,0 +1,7 @@
+public class SubstringExtraction {
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        String substring = str.substring(0, 5); // Extracts "Hello"
+        System.out.println(substring);
+    }
+}
