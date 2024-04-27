21b3b2cd3f767ac782fd25ca8e24432f3dd852e0
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-04-27 13:10:22 +0530

    Create CharacterReplacement in string

---
+++ Strings_12/src/CharacterReplacement.java
@@ -0,0 +1,7 @@
+public class CharacterReplacement {
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        String replaced = str.replace('o', 'a'); // Replaces 'o' with 'a'
+        System.out.println(replaced);
+    }
+}
