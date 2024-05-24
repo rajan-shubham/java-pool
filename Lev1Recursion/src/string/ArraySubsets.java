e90b22360d2dda3f25758bc1fbbd4b27d896bc96
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-05-24 14:54:36 +0530

    Array subset, string Permutation

--- Lev1Recursion/src/string/ArraySubsets.java
+++ Lev1Recursion/src/string/ArraySubsets.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ArraySubsets {
     public static void main(String[] args) {
         int[] arr = {1, 2, 2};
-        List<List<Integer>> ans = subset(arr);
+        List<List<Integer>> ans = subsetDuplicate(arr);
         for (List<Integer> list : ans) {
             System.out.println(list);
         }
@@ -21,6 +21,29 @@ public class ArraySubsets {
             for (int i = 0; i < n; i++) {
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element is same, s = e + 1
+            if (i > 0 && arr[i] == arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
                 outer.add(internal);
             }
         }
