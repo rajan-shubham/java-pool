7f2082eb849c0cdae0072e14bd1e0d662d38acbd
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-07-16 18:32:50 +0530

    Doubly LInked List

---
+++ 18LinkedList/src/DLL.java
@@ -0,0 +1,48 @@
+public class DLL {
+
+    private Node head; // no one can access from outside
+
+    public void insertFirst(int value){
+        Node node = new Node(value); // by default, next & prev is pointing to null;
+        node.next = head;
+        node.prev = null;
+        if (head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void display(){
+        Node node = head;
+        Node last = null;
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("Print in Reverse");
+        while (last != null){
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
--- 18LinkedList/src/Main.java
+++ 18LinkedList/src/Main.java
@@ -1,19 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(100, 3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+//        System.out.println(list.find(3));
+
+        DLL list = new DLL();
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
-        list.insertLast(99);
-        list.insert(100, 3);
+
         list.display();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(2));
-        list.display();
-        System.out.println(list.find(3));
     }
 }
