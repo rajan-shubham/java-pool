ddb93d1d6d6a3260307e084edc7a33a88676c04a
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-07-17 06:31:36 +0530

    Doubly LInked List (insert at last)

--- 18LinkedList/src/DLL.java
+++ 18LinkedList/src/DLL.java
@@ -30,6 +30,26 @@ public class DLL {
         System.out.println("START");
     }
 
+    public void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head;
+
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = node;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = node;
+        node.prev = last;
+    }
+
     private class Node {
         int value;
         Node next;
