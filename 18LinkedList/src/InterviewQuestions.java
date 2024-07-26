3da7dbe147c982fe9027154b8e3bd021f6afe169
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-07-26 08:33:31 +0530

    Q202 Happy Number ?

--- 18LinkedList/src/InterviewQuestions.java
+++ 18LinkedList/src/InterviewQuestions.java
@@ -72,6 +72,33 @@ public class InterviewQuestions {
         return s;
     }
 
+    // https://leetcode.com/problems/happy-number/
+    public boolean isHappy(int n){
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1){
+            return true;
+        }
+
+        return false;
+    }
+
+    private int findSquare(int number){
+        int ans = 0;
+        while (number != 0){
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+
     class ListNode {
         int val;
         ListNode next;
