a5388bab15f25366247fabd22b263677888271aa
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-08-08 08:20:21 +0530

    coustom stack Implementation(push,pop,peek)

--- stack-n-queues/src/CoustomStack.java
+++ stack-n-queues/src/CoustomStack.java
@@ -2,6 +2,8 @@ public class CoustomStack {
     protected int[] data;
     private static final int DEFAULT_SIZE = 10;
 
+    int top = -1;
+
     public CoustomStack() {
 //        this.data = new int[DEFAULT_SIZE];
         this(DEFAULT_SIZE);
@@ -9,5 +11,41 @@ public class CoustomStack {
 
     public CoustomStack(int size) {
         this.data = new int[size];
+    }
+
+    public boolean push(int item){
+        if (isFull()){
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        top++;
+        data[top] = item;
+        return true;
+    }
+
+    public int pop() throws StackException {
+        if (isEmpty()){
+            throw new StackException("Stack Underflow");
+        }
+//        int poppedElem = data[top];
+//        top--;
+//        return poppedElem;
+        return data[top--];
+    }
+
+    public int peek() throws StackException {
+        if (isEmpty()){
+            throw new StackException("Cannot peek from an empty stack!!");
+        }
+        return data[top];
+    }
+
+    private boolean isFull() {
+        return top == data.length - 1; // ptr is at last index
+    }
+
+    private boolean isEmpty() {
+        return top == -1; // top is at -1 index
     }
 }
---
+++ stack-n-queues/src/StackMain.java
@@ -0,0 +1,17 @@
+public class StackMain {
+    public static void main(String[] args) throws StackException{
+        CoustomStack stack = new CoustomStack(5);
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+    }
+}
