ba1bee605eb49900b0bded9edbb6633ae3c10a01
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-09-12 12:20:07 +0530

    finding middle of the node in LL

---
+++ 18LinkedList/src/MiddleNodeLL.java
@@ -0,0 +1,13 @@
+class MiddleNodeLL {
+    int getMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+     
+            while(fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast  = fast.next.next;
+            }
+
+        return slow.data;
+    }
+}
