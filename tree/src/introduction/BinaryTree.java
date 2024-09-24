5324e09917c6e2c1e9afb409b442c567e8b7d54e
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-09-24 17:02:41 +0530

    tree Data Structure

---
+++ tree/src/introduction/BinaryTree.java
@@ -0,0 +1,19 @@
+package introduction;
+
+public class BinaryTree {
+    public BinaryTree() {
+
+    }
+
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+}
