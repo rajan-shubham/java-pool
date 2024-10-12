502749ba2987f4f58927d8d8faac462d8f152ab5
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-10-12 15:18:35 +0530

    preety printing in BinaryTree

--- tree/src/introduction/BinaryTree.java
+++ tree/src/introduction/BinaryTree.java
@@ -74,3 +74,25 @@ public class BinaryTree {
     display(node.left, indent + "\t");
     display(node.right, indent + "\t");
   }
+
+public void prettyDisplay() {
+    prettyDisplay(root, 0);
+  }
+
+  private void prettyDisplay(Node node, int level) {
+    if (node == null) {
+      return;
+    }
+
+    prettyDisplay(node.right, level + 1);
+
+    if (level != 0) {
+      for (int i = 0; i < level - 1; i++) {
+        System.out.print("|\t\t");
+      }
+      System.out.println("|------->" + node.value);
+    } else {
+      System.out.println(node.value);
+    }
+    prettyDisplay(node.left, level + 1);
+  }
