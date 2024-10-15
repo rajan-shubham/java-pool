6aa775d8a6048fa202916367b53a032b776a084e
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-10-15 11:52:56 +0530

    pre order in BinaryTree

--- tree/src/introduction/BinaryTree.java
+++ tree/src/introduction/BinaryTree.java
@@ -96,3 +96,20 @@ public void prettyDisplay() {
     }
     prettyDisplay(node.left, level + 1);
   }
+
+public void preOrder() {
+    preOrder(root);
+  }
+
+  private void preOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    System.out.print(node.value + " ");
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public void inOrder() {
+    preOrder(root);
+  }
