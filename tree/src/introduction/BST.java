472fc3d300e363e97ed1dd19e6837c42bd4fed71
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-10-21 16:56:04 +0530

    Binary search tree

---
+++ tree/src/introduction/BST.java
@@ -0,0 +1,65 @@
+package introduction;
+
+public class BST {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST(){};
+
+    public int height(Node node){
+        if (node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+    private Node insert(int value, Node node){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+
+        if(value < node.value){
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value){
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public boolean balenced(){
+        return balenced(root);
+    }
+    private boolean balenced(Node node){
+        if(node == null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balenced(node.left) && balenced(node.right);
+    }
+}
