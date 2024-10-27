0461c5eb0a865a49ef1226fe9cd387fd82ff59ba
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-10-27 13:00:12 +0530

    Breadth First Search

---
+++ tree/src/questions/BFS.java
@@ -0,0 +1,73 @@
+package questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFS {
+
+//      Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+    // https://leetcode.com/problems/binary-tree-level-order-traversal/
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lot = new ArrayList<>();
+
+        if(root == null){
+            return lot;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for(int i=0; i<levelSize; i++){
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            lot.add(currentLevel);
+        }
+        return lot;
+    }
+
+    // 0ms runtiome BFS
+    /*
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        build(root, 0, result);
+        return result;
+    }
+
+    private void build(TreeNode node, int level, List<List<Integer>> result) {
+        if (node == null) return;
+
+        if (level == result.size()) result.add(new ArrayList<>());
+
+        result.get(level).add(node.val);
+
+        build(node.left, level + 1, result);
+        build(node.right, level + 1, result);
+    }
+     */
+}
