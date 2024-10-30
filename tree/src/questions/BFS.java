414aca5028916994a739400c0e609476ef006cf2
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-10-30 19:31:27 +0530

    right side view of a tree

--- tree/src/questions/BFS.java
+++ tree/src/questions/BFS.java
@@ -70,4 +70,35 @@ public class TreeNode {
         build(node.right, level + 1, result);
     }
      */
+
+    public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+
+      for (int i=0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+
+        if (i == levelSize - 1) {
+          result.add(currentNode.val);
+        }
+        
+        if (currentNode.left != null) {
+          queue.offer(currentNode.left);
+        }
+        if (currentNode.right != null) {
+          queue.offer(currentNode.right);
+        }
+      }
+    }
+    return result;
+  }
 }
