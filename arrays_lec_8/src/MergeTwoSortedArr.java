68df6c1a1d206bced500b4f7cea66777f300b262
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-11-10 10:10:04 +0530

    MergeTwoSortedArray with distinct array

---
+++ arrays_lec_8/src/MergeTwoSortedArr.java
@@ -0,0 +1,12 @@
+class MergeTwoSortedArr {
+  // https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-with-distinct-elements/1
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        Set<Integer> set = new TreeSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : a)  set.add(i);
+        for(int j : b)  set.add(j);
+        for(int i : set) list.add(i);
+        return list;
+    }
+}
