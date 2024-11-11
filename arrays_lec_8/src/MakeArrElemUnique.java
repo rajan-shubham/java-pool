cb3be9c21909d03ba652176bf10b6e8b887a66fb
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-11-11 16:22:01 +0530

    MakeArrElemUnique

---
+++ arrays_lec_8/src/MakeArrElemUnique.java
@@ -0,0 +1,21 @@
+class MakeArrElemUnique {
+
+// https://www.geeksforgeeks.org/problems/make-array-elements-unique--170645/1
+  
+    public int minIncrements(int[] arr) {
+        // Code here
+        int N = arr.length;
+        long count = 0;
+        Arrays.sort(arr);
+        for(int i = 1; i < N; i++)
+        {
+            if(arr[i] <= arr[i-1])
+            {
+                int val = arr[i-1] - arr[i] + 1;
+                count += val;
+                arr[i] += val;
+            }
+        }
+        return (int) count;
+    }
+}
