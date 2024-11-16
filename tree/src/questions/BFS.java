6dcbf51f87fbb123804c70995aa4739f6d1451da
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-16 12:51:37 +0530

    find level order successor of bt

--- tree/src/questions/BFS.java
+++ tree/src/questions/BFS.java
@@ -165,4 +165,25 @@ public class TreeNode {
     } */
 
 
+    // to find the successor of a node value in level order traversal
+    public TreeNode findSuccessor(TreeNode root, int key){
+        if (root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            TreeNode currentNode = queue.poll();
+            if (currentNode.left != null){
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null){
+                queue.offer(currentNode.right);
+            }
+            if (currentNode.val == key) break;
+        }
+
+        return queue.peek();
+    }
+
 }
