a597ec4334acfebcd726ce4b0519bd21bff3984e
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-17 10:26:45 +0530

    zigzagLO, LOBottomToTop

--- tree/src/questions/BFS.java
+++ tree/src/questions/BFS.java
@@ -1,9 +1,6 @@
 package questions;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
 
@@ -184,6 +181,76 @@ public class TreeNode {
         }
 
         return queue.peek();
+    }
+
+//    https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/submissions/1454146169/
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean reverse = false;
+
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                if (!reverse){
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null){
+                        queue.addLast(currentNode.left);
+                    }
+                    if (currentNode.right != null){
+                        queue.addLast(currentNode.right);
+                    }
+                }else{
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null){
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null){
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+//    https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> lot = new ArrayList<>();
+
+        if(root == null){
+            return lot;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for(int i=0; i<levelSize; i++){
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            lot.add(0, currentLevel);
+        }
+        return lot;
     }
 
 }
