38ffb6643678c374c291cca98b37bdf1f8b99b74
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-19 15:50:41 +0530

    whether the given two nodes are cousion or not

--- tree/src/questions/BFS.java
+++ tree/src/questions/BFS.java
@@ -1,5 +1,7 @@
 package questions;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class BFS {
@@ -273,5 +275,42 @@ public class TreeNode {
         }
         return root;
     }
+
+//    https://leetcode.com/problems/cousins-in-binary-tree/
+    public boolean isCousins(TreeNode root, int x, int y){
+        TreeNode xx = findNode(root, x);
+        TreeNode yy = findNode(root, y);
+
+        return (level(root, xx, 0) == level(root, yy, 0) && (!isSibling(root, xx, yy)));
+    }
+    TreeNode findNode(TreeNode node, int x){
+        if (node == null) return null;
+
+        if (node.val == x) return node;
+
+        TreeNode n = findNode(node.left, x);
+        if (n != null) return n;
+
+        return findNode(node.right, x);
+    }
+    boolean isSibling(TreeNode node, TreeNode x, TreeNode y){
+        if (node == null) return false;
+
+        return (
+                (node.left == x && node.right == y) || (node.left == y && node.right == x) || isSibling(node.left, x, y) || isSibling(node.right, x, y)
+                );
+    }
+    int level(TreeNode node, TreeNode x, int lev){
+        if (node == null) return 0;
+
+        if (node == x) return lev;
+
+        int l = level(node.left, x, lev+1);
+        if (l != 0) return l;
+
+        return level(node.right, x, lev+1);
+    }
+
+
 
 }
