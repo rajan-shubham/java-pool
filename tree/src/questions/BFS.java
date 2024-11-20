6ca066853ddccff61dabc4c7521a5639417accaf
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-20 12:36:37 +0530

    tree is symmytric or not

--- tree/src/questions/BFS.java
+++ tree/src/questions/BFS.java
@@ -312,5 +312,27 @@ public class TreeNode {
     }
 
 
+    public boolean isSymmetric(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if (left == null && right == null) continue;
+
+            if (left == null || right == null) return false;
+
+            if (left.val != right.val) return false;
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
 
 }
