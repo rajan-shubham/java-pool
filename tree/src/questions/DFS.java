658c1e07a9f84d7d250c597d2db1dd9c8ff36bc4
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-23 10:09:50 +0530

    binaryTree to linkedList(skewedBT) using preOrder

--- tree/src/questions/DFS.java
+++ tree/src/questions/DFS.java
@@ -113,4 +113,22 @@ public class DFS {
         populateSorted(nums, mid+1, end);
     }
 
+//    https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+    public void flatten(TreeNode root){
+        TreeNode current = root;
+        while (current != null){
+            if (current.left != null){
+                TreeNode temp = current.left;
+                while (temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
+            current = current.right;
+        }
+    }
+
+
 }
