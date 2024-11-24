290c90f83a982278d8580d6b4f8d8fb8a817dc58
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-24 10:21:57 +0530

    validate BST or not

--- tree/src/questions/DFS.java
+++ tree/src/questions/DFS.java
@@ -130,5 +130,20 @@ public class DFS {
         }
     }
 
+    public boolean isValidBST(TreeNode root){
+        return helper(root, null, null);
+    }
+    public boolean helper(TreeNode node, Integer low, Integer high){
+        if (node == null) return true;
+
+        if (low != null && node.val <= low) return false;
+
+        if (high != null && node.val >= high) return false;
+
+        boolean leftTree = helper(node.left, low, node.val);
+        boolean rightTree = helper(node.right, node.val, high);
+
+        return leftTree && rightTree;
+    }
 
 }
