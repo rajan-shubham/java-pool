fd1de95299e02fa7e1753886c17b7f732980a5b0
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-25 17:03:24 +0530

    LowestCommonAncestor

--- tree/src/questions/DFS.java
+++ tree/src/questions/DFS.java
@@ -146,4 +146,18 @@ public class DFS {
         return leftTree && rightTree;
     }
 
+//    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) return root;
+
+        return left == null ? right : left;
+    }
+
 }
