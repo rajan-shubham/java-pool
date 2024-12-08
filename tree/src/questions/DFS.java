3a245a81c0bd699db080c0ff2c9277fd4d8c41ec
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-12-08 23:49:48 +0530

    Construct Binary Tree from Preorder and Inorder Traversal

--- tree/src/questions/DFS.java
+++ tree/src/questions/DFS.java
@@ -200,5 +200,29 @@ public class DFS {
         return kthSmallest0ms(root.right, k);
     }
 
+//    https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if (preorder.length == 0) {
+            return null;
+        }
+
+        int r = preorder[0];
+        int index = 0;
+
+        for(int i=0; i<inorder.length; i++) {
+            if(inorder[i] == r) {
+                index = i;
+            }
+        }
+
+        TreeNode node = new TreeNode(r);
+
+        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+
+        return node;
+
+    }
 
 }
