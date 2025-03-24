9ed594483e64ae5f992e45672539a208bf13238b
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-03-24 18:14:37 +0530

    doubly linked list from BT(inOrder)

---
+++ tree/src/advanceTreeQuest/DoubleLinkedList.java
@@ -0,0 +1,48 @@
+package advanceTreeQuest;
+
+public class DoubleLinkedList {
+    LLNode head;
+    LLNode tail;
+
+    public LLNode convert(TreeNode root){
+        if (root == null) return null;
+        helper(root);
+        return head;
+    }
+    private void helper(TreeNode node){
+        if (node == null) return;
+        helper(node.left);
+        LLNode newNode = new LLNode(node.val);
+
+        if (head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        helper(node.right);
+    }
+}
+
+class LLNode {
+    int val;
+    LLNode prev;
+    LLNode next;
+
+    public LLNode(int val){
+        this.val = val;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode (int val){
+        this.val = val;
+    }
+}
