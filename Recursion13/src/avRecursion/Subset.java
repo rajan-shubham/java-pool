0985393c47f413b4332a40b1ece50b3d7401c546
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-03-27 11:58:06 +0530

    find all subset of string

---
+++ Recursion13/src/avRecursion/Subset.java
@@ -0,0 +1,36 @@
+package avRecursion;
+
+import java.util.ArrayList;
+
+public class Subset {
+    static ArrayList<String> subsets = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String ip = "abc";
+        String op = "";
+        findSubset(ip, op);
+        findSubset2(ip, op);
+
+        System.out.println(subsets);
+    }
+
+    // for finding the subset recursively (selecting of not selecting for a char in input string)
+    public static void findSubset(String ip, String op){
+        if (ip.isEmpty()){ // when input string is empty or you are at leaf node
+            System.out.print(op + " ");
+            return;
+        }
+        findSubset(ip.substring(1), op); // decreasing ip string, not adding first char to output string
+        findSubset(ip.substring(1), op + ip.charAt(0)); // decreasing ip string, add first char of ip string to op string
+    }
+
+    public static void findSubset2(String ip, String op){
+        if (ip.isEmpty()){
+            subsets.add(op);
+            return;
+        }
+        char ch = ip.charAt(0);
+        findSubset2(ip.substring(1), op);
+        findSubset2(ip.substring(1), op + ch);
+    }
+}
