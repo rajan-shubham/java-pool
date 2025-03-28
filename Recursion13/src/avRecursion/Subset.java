b3bc8c809428b18fb81c7e2984e5e42a70aa2477
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-03-28 19:40:06 +0530

    find Unique subset

--- Recursion13/src/avRecursion/Subset.java
+++ Recursion13/src/avRecursion/Subset.java
@@ -1,17 +1,20 @@
 package avRecursion;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Subset {
     static ArrayList<String> subsets = new ArrayList<>();
 
     public static void main(String[] args) {
-        String ip = "abc";
+        String ip = "aab";
         String op = "";
         findSubset(ip, op);
         findSubset2(ip, op);
+        findUniqueSubset(ip, op);
 
+        System.out.println();
         System.out.println(subsets);
+        System.out.println(set);
     }
 
     // for finding the subset recursively (selecting of not selecting for a char in input string)
@@ -32,5 +35,15 @@ public class Subset {
         char ch = ip.charAt(0);
         findSubset2(ip.substring(1), op);
         findSubset2(ip.substring(1), op + ch);
+    }
+
+    static HashSet<String> set = new HashSet<>();
+    public static void findUniqueSubset(String ip, String op){
+        if (ip.isEmpty()){
+            set.add(op);
+            return;
+        }
+        findUniqueSubset(ip.substring(1), op);
+        findUniqueSubset(ip.substring(1), op + ip.charAt(0));
     }
 }
